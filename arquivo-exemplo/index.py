c5b9b3741d0fba3f58599db50f3b229e4a48a845
Cleverton Sampaio Ramos <52421999+clevertonsampaio@users.noreply.github.com>
2025-09-24 10:22:49 -0300

    test

--- arquivo-exemplo/index.py
+++ arquivo-exemplo/index.py
@@ -7,3 +7,8 @@ print("Adicionando mais funcionalidades ao script.")
 print("Finalizando o script com uma mensagem de despedida.")
 print("Até mais!")  
 print("Obrigado por usar este script!")
+
+
+s = 1 + 2
+print(s)
+print("A soma de 1 + 2 é:", s) 
