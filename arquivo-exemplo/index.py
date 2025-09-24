fe9997a60225388c7b936d3099bb2e5a27cbacfc
Cleverton Sampaio Ramos <52421999+clevertonsampaio@users.noreply.github.com>
2025-09-24 10:26:54 -0300

    Update index.py

--- arquivo-exemplo/index.py
+++ arquivo-exemplo/index.py
@@ -1,16 +1,3 @@
 # Meu primeiro script Python em repositório
 print("Olá, mundo!")
 
-print("Este é um exemplo de script Python.")
-print("Adicionando mais funcionalidades ao script.")
-
-print("Finalizando o script com uma mensagem de despedida.")
-print("Até mais!")  
-print("Obrigado por usar este script!")
-
-
-s = 1 + 2
-print(s)
-print("A soma de 1 + 2 é:", s) 
-
-print('Olha lá')
